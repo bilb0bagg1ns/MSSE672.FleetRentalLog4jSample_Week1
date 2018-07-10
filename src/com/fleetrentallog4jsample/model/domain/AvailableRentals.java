16a912e7ea9e035da3bfe2f957b517c830eebe24
bilbobagg1ns <mprasad@regis.edu>
2018-07-10 13:22:14 -0600

    Formatting

--- src/com/fleetrentallog4jsample/model/domain/AvailableRentals.java
+++ src/com/fleetrentallog4jsample/model/domain/AvailableRentals.java
@@ -4,122 +4,122 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+public class AvailableRentals implements Serializable {
 
-public class AvailableRentals implements Serializable
-{
-    /** Holds state if rentals are available or not */
-	private boolean available;
+  /**
+   * Holds state if rentals are available or not
+   */
+  private boolean available;
 
-	/** state tax associated with this rental */
-	private float stateTax;
-	
-	/** holds car objects */
-	private ArrayList<Car> availableRentalsList = new ArrayList<Car>(1);
-	
-	/**ArrayList is not synchronized, so if you need multi-threaded access, consider using: */
-	//List availableRentalsList = Collections.synchronizedList(new ArrayList(...)); 
+  /**
+   * state tax associated with this rental
+   */
+  private float stateTax;
 
+  /**
+   * holds car objects
+   */
+  private ArrayList<Car> availableRentalsList = new ArrayList<Car>(1);
 
-	/**
-	 *
-	 */
-	public AvailableRentals() {
+  /**
+   * ArrayList is not synchronized, so if you need multi-threaded access,
+   * consider using:
+   */
+  //List availableRentalsList = Collections.synchronizedList(new ArrayList(...)); 
+  /**
+   *
+   */
+  public AvailableRentals() {
 
-		// TODO Auto-generated constructor stub
-	}
-	
-	/**
-	 * @param available
-	 */
-	public AvailableRentals(boolean available) {
-		super();
-		this.available = available;
-	}
+    // TODO Auto-generated constructor stub
+  }
 
-	/**
-	 * @param available
-	 */
-	public AvailableRentals(boolean available, float stateTax) 
-	{
-		super();
-		this.available = available;
-		this.stateTax = stateTax;
-	}
-	
-	/**
-	 *  Add each available rental car into the ArrayList
-	 *  
-	 * @param car
-	 */
-	public void addRental(Car car)
-	{
-		availableRentalsList.add(car);
-	}
+  /**
+   * @param available
+   */
+  public AvailableRentals(boolean available) {
+    super();
+    this.available = available;
+  }
 
-	
-	/**
-	 * @return Returns the available.
-	 */
-	public boolean isAvailable() {
-		return available;
-	}
-	/**
-	 * @param available The available to set.
-	 */
-	public void setAvailable(boolean available) {
-		this.available = available;
-	}
+  /**
+   * @param available
+   */
+  public AvailableRentals(boolean available, float stateTax) {
+    super();
+    this.available = available;
+    this.stateTax = stateTax;
+  }
 
-	
-	/**
-	 * @param stateTax The stateTax to set.
-	 */
-	public void setStateTax(float stateTax) {
-		this.stateTax = stateTax;
-	}
+  /**
+   * Add each available rental car into the ArrayList
+   *
+   * @param car
+   */
+  public void addRental(Car car) {
+    availableRentalsList.add(car);
+  }
 
-	/**
-	 * @return Returns the stateTax.
-	 */
-	public float getStateTax() {
-		return stateTax;
-	}
+  /**
+   * @return Returns the available.
+   */
+  public boolean isAvailable() {
+    return available;
+  }
 
-	/**
-	 * @return Returns the availableRentalsList.
-	 */
-	public ArrayList getAvailableRentalsList() {
-		return availableRentalsList;
-	}
+  /**
+   * @param available The available to set.
+   */
+  public void setAvailable(boolean available) {
+    this.available = available;
+  }
 
-	/**
-	 * @param availableRentalsList The availableRentalsList to set.
-	 */
-	public void setAvailableRentalsList(ArrayList availableRentalsList) {
-		this.availableRentalsList = availableRentalsList;
-	}
+  /**
+   * @param stateTax The stateTax to set.
+   */
+  public void setStateTax(float stateTax) {
+    this.stateTax = stateTax;
+  }
 
-	public String toString()
-	{
-	  StringBuffer strBfr = new StringBuffer();
-	  
-	  if (available)
-	  {	  
-	    strBfr.append("Rental is available");
-     strBfr.append("\nState Tax: ");
-     strBfr.append(stateTax);
-     strBfr.append("\nAvailable Rentals List: ");
-		
-	    Iterator iterator= availableRentalsList.iterator();
-	    while (iterator.hasNext())
-	    {
-    	  strBfr.append((Car)iterator.next());
-     }
-	  }	  
-	  else
-	    strBfr.append("No Rental is available");
+  /**
+   * @return Returns the stateTax.
+   */
+  public float getStateTax() {
+    return stateTax;
+  }
 
-	  return strBfr.toString();
-	}
+  /**
+   * @return Returns the availableRentalsList.
+   */
+  public ArrayList getAvailableRentalsList() {
+    return availableRentalsList;
+  }
+
+  /**
+   * @param availableRentalsList The availableRentalsList to set.
+   */
+  public void setAvailableRentalsList(ArrayList availableRentalsList) {
+    this.availableRentalsList = availableRentalsList;
+  }
+
+  public String toString() {
+    StringBuffer strBfr = new StringBuffer();
+
+    if (available) {
+      strBfr.append("Rental is available");
+      strBfr.append("\nState Tax: ");
+      strBfr.append(stateTax);
+      strBfr.append("\nAvailable Rentals List: ");
+
+      Iterator iterator = availableRentalsList.iterator();
+      while (iterator.hasNext()) {
+        strBfr.append((Car) iterator.next());
+      }
+    } else {
+      strBfr.append("No Rental is available");
+    }
+
+    return strBfr.toString();
+  }
 
 } // end class RentalAvail
